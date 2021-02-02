a168201827d3fa9d8661545bc5b22adf9f98536b
Math <70871643+Math-Cloud@users.noreply.github.com>
2021-02-02 11:32:07 -0500

    Main

---
+++ exercices/src/main/java/ca/csfoy/servicesweb/exercices/exercices/ExercicesApplication.java
@@ -0,0 +1,13 @@
+package ca.csfoy.servicesweb.exercices.exercices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicesApplication.class, args);
+	}
+
+}
---
+++ exercices/src/test/java/ca/csfoy/servicesweb/exercices/exercices/ExercicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.csfoy.servicesweb.exercices.exercices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
