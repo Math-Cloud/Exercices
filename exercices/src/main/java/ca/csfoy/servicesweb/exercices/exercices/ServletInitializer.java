4f2d90cfa0fbbd23b52abd82dd32e0e709657135
Math <70871643+Math-Cloud@users.noreply.github.com>
2021-02-02 13:43:54 -0500

    Setup Branch Exercice 1

---
+++ exercices/src/main/java/ca/csfoy/servicesweb/exercices/exercices/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ca.csfoy.servicesweb.exercices.exercices;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ExercicesApplication.class);
+	}
+
+}
